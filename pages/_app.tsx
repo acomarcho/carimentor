f9f827d6f9cc91c6b36e653f5441978fdb8fa8d3
acomarcho <acoxstpd@gmail.com>
2023-07-21 19:45:59 +0700

    Feat: Initialize tailwind configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+});
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-plus-jakarta-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component
+        className={`${outfit.variable} ${plusJakartaSans.variable}`}
+        {...pageProps}
+      />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        outfit: ["var(--font-outfit)"],
+        jakarta: ["var(--font-plus-jakarta-sans)"],
+      },
+      colors: {
+        purple: {
+          50: "#f2f0ff",
+          600: "#6254d9",
+          950: "#120b53",
+        },
+        gray: {
+          25: "#fdfdfd",
+          100: "#eceeef",
+          400: "#ced4da",
+        },
       },
     },
   },
   plugins: [],
-}
+};
