9ce57d9d077aebe50b0be218406c5721955b3c4c
acomarcho <acoxstpd@gmail.com>
2023-07-21 19:53:14 +0700

    Feat: Add mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
 
@@ -14,8 +16,19 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${outfit.variable} ${plusJakartaSans.variable}`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <main className={`${outfit.variable} ${plusJakartaSans.variable}`}>
+          <Component {...pageProps} />
+        </main>
+      </MantineProvider>
+    </>
   );
 }
