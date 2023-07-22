29bbe1148d57963acd1cb38b20624df38ac4fb41
acomarcho <acoxstpd@gmail.com>
2023-07-22 15:03:04 +0700

    Feat: Initialize page

---
+++ components/mentor/group-session/index.tsx
@@ -0,0 +1,10 @@
+import DecorationVector from "@/components/common/decoration-vector";
+
+export default function MentorGroupSessions() {
+  return (
+    <div className="default-wrapper">
+      <DecorationVector />
+      <h1 className="header-2rem underline">Sesi Grup Anda</h1>
+    </div>
+  );
+}
