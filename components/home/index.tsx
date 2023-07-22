e4c53501c15371d74a749d56f927488f7373cf05
acomarcho <acoxstpd@gmail.com>
2023-07-22 07:01:09 +0700

    Feat: Add filter

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -3,6 +3,8 @@ import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
 import { useState } from "react";
 import { dummyTags } from "@/lib/dummies";
+import { Radio, Checkbox, MultiSelect } from "@mantine/core";
+import { labelStyle } from "@/lib/constants/styles";
 
 export default function Home() {
   const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
@@ -29,6 +31,44 @@ export default function Home() {
       >
         <div className="max-w-[1160px] mx-auto p-[1.25rem]">
           <h1 className="header-2rem">Filter pencarian</h1>
+          <div className="flex flex-col gap-[0.5rem] mt-[1rem]">
+            <h2 className="filter-subheader">Lokasi</h2>
+            <Radio.Group className="flex flex-col items-start gap-[0.5rem]">
+              <Radio
+                value="NONE"
+                label="Semua tempat"
+                styles={{ ...labelStyle }}
+              />
+              <Radio
+                value="PROVINCE"
+                label="Provinsi yang sama"
+                styles={{ ...labelStyle }}
+              />
+              <Radio
+                value="CITY"
+                label="Kota yang sama"
+                styles={{ ...labelStyle }}
+              />
+            </Radio.Group>
+          </div>
+          <div className="flex flex-col gap-[0.5rem] mt-[1rem]">
+            <h2 className="filter-subheader">Status mentor</h2>
+            <Checkbox label="Premium saja" styles={{ ...labelStyle }} />
+          </div>
+          <div className="flex flex-col gap-[0.5rem] mt-[1rem]">
+            <h2 className="filter-subheader">Ketertarikan</h2>
+            <MultiSelect
+              radius="xl"
+              data={tags.map((tag) => {
+                return {
+                  value: tag.id,
+                  label: tag.name,
+                };
+              })}
+              placeholder="Pilih semua yang Anda sukai!"
+              clearable
+            />
+          </div>
         </div>
       </Drawer>
     </div>
