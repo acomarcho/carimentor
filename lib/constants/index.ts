371a84ada2aa7ac15b049fa12a4477e8139dff6e
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-07-22 21:16:25 +0700

    fix: resolve api url

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,7 +3,7 @@ export const appName = "CariMentor";
 const env: string = "prod";
 
 export const apiURL =
-  env === "prod"
+  env === "local"
     ? "http://localhost:9999"
     : "https://garudahack-api.malikrafsan.tech";
 export const imgbbApiKey = "3e75de76703676728048f1f97e5943c5";
