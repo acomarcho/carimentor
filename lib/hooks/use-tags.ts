e36dcbd610d904dd171cb77e667d92f9d7bec2ac
Maria Khelli <13520115@std.stei.itb.ac.id>
2023-07-22 18:49:50 +0700

    fix: naming responses

--- lib/hooks/use-tags.ts
+++ lib/hooks/use-tags.ts
@@ -1,17 +1,14 @@
-import useSWR from "swr";
 import { apiURL } from "@/lib/constants";
-import { GetTagListResponse } from "../constants/responses";
+import useSWR from "swr";
+import { GetTagDataResponse } from "../constants/responses";
 
 export function useTags() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(
-    `${apiURL}/tag/list`,
-    fetcher
-  );
+  const { data, error, isLoading } = useSWR(`${apiURL}/tag/list`, fetcher);
 
   return {
-    tags: data as GetTagListResponse,
+    tags: data as GetTagDataResponse,
     isLoading,
     isError: error,
   };
