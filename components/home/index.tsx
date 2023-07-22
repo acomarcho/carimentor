82e0ae0499464a70c826a6b9483bae1850e8a7dd
acomarcho <acoxstpd@gmail.com>
2023-07-22 07:44:34 +0700

    Feat: Add text-center

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -77,8 +77,8 @@ export default function Home() {
   return (
     <div className="default-wrapper flex flex-col gap-[1rem] items-center justify-center">
       <DecorationVector />
-      <h1 className="header-2rem">Ingin belajar apa Anda hari ini?</h1>
-      <p className="paragraph">Cari mentor yang sesuai dengan kebutuhanmu!</p>
+      <h1 className="header-2rem text-center">Ingin belajar apa Anda hari ini?</h1>
+      <p className="paragraph text-center">Cari mentor yang sesuai dengan kebutuhanmu!</p>
       <button
         className="button-600-filled max-w-form"
         onClick={() => setIsFilterOpen(true)}
