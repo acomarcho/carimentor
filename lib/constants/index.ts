332eb149396818cf92d394a479894f22a24ad6aa
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-07-22 17:27:30 +0700

    feat: add use user

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,1 +1,9 @@
 export const appName = "CariMentor";
+const env: string = "local";
+
+export const apiURL =
+  env === "local"
+    ? "http://localhost:9999"
+    : "https://garudahack-api.malikrafsan.tech";
+export const imgbbApiKey = "3e75de76703676728048f1f97e5943c5";
+
