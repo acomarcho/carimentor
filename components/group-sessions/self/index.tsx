026115db8a349e31d4a1dae8335b8e44cc1d5d9e
acomarcho <acoxstpd@gmail.com>
2023-07-22 14:36:15 +0700

    Feat: Initialize joined sessions page

---
+++ components/group-sessions/self/index.tsx
@@ -0,0 +1,10 @@
+import DecorationVector from "@/components/common/decoration-vector";
+
+export default function MyGroupSessions() {
+  return (
+    <div className="default-wrapper">
+      <DecorationVector />
+      <h1 className="header-2rem underline">Riwayat Sesi Grup</h1>
+    </div>
+  );
+}
