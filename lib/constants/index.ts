21815c8ea04a748a343483ea3faa516d02192024
acomarcho <acoxstpd@gmail.com>
2023-07-22 17:52:51 +0700

    Feat: Use production API

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,10 +1,9 @@
 export const appName = "CariMentor";
 
-const env: string = "local";
+const env: string = "prod";
 
 export const apiURL =
   env === "local"
     ? "http://localhost:9999"
     : "https://garudahack-api.malikrafsan.tech";
 export const imgbbApiKey = "3e75de76703676728048f1f97e5943c5";
-
